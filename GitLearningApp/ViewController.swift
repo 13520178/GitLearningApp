76c12bba461fb399e6a52469e91e88fabfef864d
Phan Dang <phannhatd@gmail.com>
2025-04-03 09:19:45 +0700

    Thêm màn hình thứ 2 và nút chuyển

--- GitLearningApp/ViewController.swift
+++ GitLearningApp/ViewController.swift
@@ -18,10 +18,21 @@ class ViewController: UIViewController {
         button.frame = CGRect(x: 100, y: 200, width: 200, height: 50)
         button.addTarget(self, action: #selector(handleButtonTap), for: .touchUpInside)
         view.addSubview(button)
+        
+        let nextButton = UIButton(type: .system)
+        nextButton.setTitle("Mở màn 2", for: .normal)
+        nextButton.frame = CGRect(x: 100, y: 300, width: 200, height: 50)
+        nextButton.addTarget(self, action: #selector(openSecondScreen), for: .touchUpInside)
+        view.addSubview(nextButton)
     }
 
     @objc func handleButtonTap() {
         print("Nút đã được nhấn")
+    }
+    
+    @objc func openSecondScreen() {
+        let vc = SecondViewController()
+        navigationController?.pushViewController(vc, animated: true)
     }
 
 }
