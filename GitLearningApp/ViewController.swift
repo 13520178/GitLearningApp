7ef024d41eceba8f8f181ef22fcae9df1ca4cb40
Phan Dang <phannhatd@gmail.com>
2025-04-03 08:58:05 +0700

    Thêm UIButton và xu lý sự kiện nhấn

--- GitLearningApp/ViewController.swift
+++ GitLearningApp/ViewController.swift
@@ -12,8 +12,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hello, Git!")
+        
+        let button = UIButton(type: .system)
+        button.setTitle("Nhấn vào ", for: .normal)
+        button.frame = CGRect(x: 100, y: 200, width: 200, height: 50)
+        button.addTarget(self, action: #selector(handleButtonTap), for: .touchUpInside)
+        view.addSubview(button)
     }
 
+    @objc func handleButtonTap() {
+        print("Nút đã được nhấn")
+    }
 
 }
 
